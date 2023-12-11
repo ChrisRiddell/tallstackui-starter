7d3fcce899e41dda74226f8234b962e8ef464e45
Chris Riddell <109709314+ChrisRiddell@users.noreply.github.com>
2023-12-11 21:57:40 +1000

    wip

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html
+    class="bg-gray-100 dark:bg-gray-800"
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+>
 
     <head>
         <meta charset="utf-8">
